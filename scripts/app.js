69fbeb8a62c03d3096570e1f72afb93a80a41dd4
JoshuaEd <joshuaedward196@gmail.com>
2026-08-07 22:15:10 +0100

    JS was converte to modules

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 let productsContainer = document.querySelector(".products-grid");
 
 let productHTML = "";
@@ -25,7 +27,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="product-quantity-selector-${product.id}" >
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -41,7 +43,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -54,14 +56,13 @@ products.forEach((product) => {
 });
 
 productsContainer.innerHTML += productHTML;
+//let cartQuantity = JSON.parse(localStorage.getItem("cartQuantity") || 0);
 
 document.querySelectorAll(".addtocart").forEach((button) => {
   button.addEventListener("click", () => {
     console.log("Added");
-    const productId = button.dataset.productId;
-    const productName = button.dataset.productName;
-    const productPrice = button.dataset.productPrice;
-    // const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const { productId, productName, productPrice } = button.dataset;
     let price = 0;
     let matchingItem;
 
@@ -79,18 +80,19 @@ document.querySelectorAll(".addtocart").forEach((button) => {
         productId === item.productId
       ) {
         matchingItem = item;
-        price += Number(item.productPrice * 100);
+        price += Number(item.productPrice) * 100;
         totalPrice = price;
         console.log(totalPrice);
       }
     });
+
     if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
       cart.push({
-        productId: productId,
-        productName: productName,
-        productPrice: productPrice,
+        productId,
+        productName,
+        productPrice,
         quantity: 1,
       });
     }
@@ -98,10 +100,31 @@ document.querySelectorAll(".addtocart").forEach((button) => {
     let cartQuantity = 0;
     cart.forEach((item) => {
       cartQuantity += item.quantity;
+      let selector = Number(
+        document.querySelector(`.product-quantity-selector-${productId}`).value,
+      );
+      if (selector > 1) {
+        cartQuantity = item.quantity += selector - 1;
+        // document.querySelector('.cart-quantity').innerHTML += selector;
+      }
       document.querySelector(".cart-quantity").innerHTML = cartQuantity;
-      console.log(cartQuantity);
     });
 
-    //localStorage.setItem("cart", JSON.stringify(cart));
+    const notification = document.querySelector(`.added-to-cart-${productId}`);
+    let notificationTimeoutId = false;
+    if (notification.classList.contains(`added-to-cart-${productId}`)) {
+      notification.style.opacity = 1;
+      notificationTimeoutId = true;
+      setTimeout(() => {
+        notificationTimeoutId = setTimeout(() => {
+          notification.style.opacity = 0;
+        }, 1000);
+      }, 1000);
+      clearTimeout(notificationTimeoutId);
+      // clearInterval(notificationTimeoutId);
+    }
+    notificationTimeoutId = false;
+
+    localStorage.setItem("cartQuantity", JSON.stringify(cartQuantity));
   });
 });
